c38029d712a5b1d8b84583478e7c5887975e5727
Nick Gronow <nick@wov.io>
2020-06-15 12:55:21 -0400

    Change mailgun arg name from -> sender

--- template/python/base/mailgun.py
+++ template/python/base/mailgun.py
@@ -10,7 +10,7 @@ def send(**options):
     Required keyword arguments:
     domain -- String - The mailgun domain name
     apikey -- String - Api key to access mailgun's api
-    from -- String - "name <name@domain.com>"
+    sender -- String - "name <name@domain.com>"
     to -- String - "name <name@domain.com>"
     subject -- String - Email subject
     template -- String - template slug
@@ -20,7 +20,7 @@ def send(**options):
         f'https://api.mailgun.net/v3/{options["domain"]}/messages',
         auth=("api", options['apikey']),
         data={
-            'from': options['from'],
+            'from': options['sender'],
             'to': options['to'],
             'subject': options['subject'],
             'template': options['template'],
